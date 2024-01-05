c3c2166cdd181924f75e115c6afdf6dd387e6e46
Georgiy Slanskiy <g.slanskiy@sbereducation.ru>
2024-01-06 00:02:40 +0300

    before refactor

--- source/js/app.js
+++ source/js/app.js
@@ -36,8 +36,12 @@ function addActiveLinkTablet () {
   const screenWidth = window.innerWidth
   const lastPart = currentPath()
   const element = document.querySelector(`a[href="${lastPart}"]`)
+  const allElementsExceptLastPart = headerMenuList.querySelectorAll(`a:not([href="${lastPart}"])`)
   if (screenWidth >= 768) {
-    element.classList.add('header__menu-link--active')
+    element.classList.add('header__menu-link--active');
+    allElementsExceptLastPart.forEach((aTag) => {
+      aTag.classList.add('header__menu-link--down')
+    })
   }
 }
 
